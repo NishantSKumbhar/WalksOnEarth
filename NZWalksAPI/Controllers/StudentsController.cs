9a9ba8810e6af78831c6ce6f121aa997f5edc73b
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-06-30 11:46:32 +0530

    Add project files.

---
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] StudentNames = new string[] { "John", "Merry", "Jessy", "Walter", "Gustavo" };
+            return Ok(StudentNames);
+        }
+    }
+}
---
+++ NZWalksAPI/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalksAPI.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; } // can have null values so use ?
+        public Guid DifficultyId { get; set; }
+        public Guid RedionId { get; set; }
+
+        // Navigation Property
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
