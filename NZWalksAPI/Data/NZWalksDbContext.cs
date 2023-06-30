58efdaf89c5d8de3602cbb262e08bb682d81bc96
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-06-30 11:56:08 +0530

    Db Context completed

--- NZWalksAPI/Data/NZWalksDbContext.cs
+++ NZWalksAPI/Data/NZWalksDbContext.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
 
 namespace NZWalksAPI.Data
 {
     public class NZWalksDbContext: DbContext
     {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+
+        }
+
+        // When we run entity core migrations, Below three properties will create tables in database.
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
     }
 }
